dec3fd0cfb9dc50f96bed4ae7671110ea52e451c
Urvashi Agrawal <158180920+urvashi-agrawal-dev@users.noreply.github.com>
2025-07-03 10:02:39 +0000

    ff

---
+++ twosumfirstleetcode.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] twosumfirstleetcode(int[] nums, int target) {
+        int n = nums.length;
+        for(int i = 0; i < n; i++) {
+            for(int j = i + 1; j < n; j++) {
+                if(nums[i] + nums[j] == target) {
+                    return new int[] {i, j};
+                }
+            }
+        }
+        return new int[] {};
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        int[] nums = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = sol.twosumfirstleetcode(nums, target);
+        
+        if(result.length == 2) {
+            System.out.println("Indices: " + result[0] + ", " + result[1]);
+        } else {
+            System.out.println("No solution found.");
+        }
+    }
+}
